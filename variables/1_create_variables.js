3cc9a16f1e6b44ac909ad57788b764b9c2d4dc79
sudhir kumar <65527808+titanSlayer12@users.noreply.github.com>
2021-07-22 21:39:18 +0530

    Add files via upload

---
+++ variables/1_create_variables.js
@@ -0,0 +1,75 @@
+
+
+
+
+//1. JavaScript Basics 
+//2. JavaScript DOM
+//3. functional JavaScript
+//4. Objectoriented & asyn programming 
+
+
+//.......................Create a variable......................
+
+"use strict"
+
+
+//declare a variable
+var score;
+console.log(score);
+
+var abc = 10;
+var abc ;
+console.log(abc);
+
+//declare a variable and initialize
+var livesLeft = 3;
+console.log(livesLeft);
+
+//re-assign a value to our variable livesLeft
+livesLeft = 2;
+console.log(livesLeft);
+
+//constants
+const firstname = 'John';
+console.log(firstname);
+
+
+//.....................................................
+var a = 15;
+let b = 5;
+const c = 25;
+console.log(a);   //15
+console.log(b);   // 5
+console.log(c);    // 25
+
+function func(){
+    console.log(a);   // undefined
+    var a = 6;
+    let b = 16;
+    const c = 26;
+    console.log(a);  // 6
+    console.log(b);  //16
+    console.log(c);  // 26
+    {
+        var  a = 7;
+        let b = 17;
+        const c = 27;
+        console.log(a);   //7
+        console.log(b);   // 17
+        console.log(c);   // 27
+    };
+    {
+        console.log(a);   //7
+        console.log(b);   // 16
+        console.log(c);   // 26
+    };
+}
+
+func();
+
+{
+    var a = 25;
+}
+console.log(a);       // 25
+console.log(b);        // 5
+console.log(c);       // 25
---
+++ variables/2_Numbers.js
@@ -0,0 +1,122 @@
+// Primitive vs Non-Primitive data types
+// 1. number ---------- Number
+// 2. string ---------- String
+// 3. boolean ---------- Boolean
+// 4. undefined  ...... undefined
+// 5. null ............ Object
+
+//.......................... Numbers ...........................
+
+var x = 1;
+console.log(typeof x); // "number"
+
+//fractional numbers
+var y = 2.4;
+console.log(y);
+
+//5.3 * 10^8
+var z = 5.3e8;
+console.log(z);
+
+//max min in JS
+var max = Number.MAX_VALUE;
+console.log(max);
+var min = Number.MIN_VALUE;
+console.log(min);
+
+//arithmetic operation ,+,*,-,/ , +=, -=, *=,
+var a = 4 + 9;
+console.log(a); //13
+
+a = 3 * 4;
+console.log(a); // 12
+
+a = y - x;
+console.log(a); // 1.4
+
+console.log(12 / 4);
+
+// Data type precedence
+// 1. String
+// 2. Number
+// 3. Boolean
+
+var cdedf = 10 + "11" + true + "10";
+console.log(cdedf);
+
+var cd_e = 10 + +"11" + false + +"110";
+console.log(cd_e);
+
+//...................................
+var b = 3;
+//b = b + 2;
+b += 2; // b = b + 2;
+console.log(b); // 5
+
+//b += 1; // b = b + 1
+b++;
+var a1 = b++;
+console.log(a1); //6
+console.log(b); // 7
+
+var a1 = b++;
+console.log(a1);
+console.log(b);
+
+var a2 = ++b;
+console.log(a2);
+
+//b -= 1;
+b--;
+console.log(b);
+//.............................................................................
+//operator precedence
+var c = 4 * (5 + 3);
+console.log(c); // 20
+
+var d = ++c;
+console.log(c); // 21
+console.log(d); // 21
+
+var e = c++;
+console.log(c); // 22
+console.log(e); // 21
+//..................More Operators-Special Numbers..................
+//remainder operator
+var f = 17 % 3;
+console.log(f); //17 is 3 * 5 +2
+//.......................................................................
+
+//exponent operator
+var g = 3 ** 4; //3^4: returns the base to the exponent power
+console.log(g);
+//.......................................................................
+
+//Infinity
+var h = Infinity;
+console.log(h);
+console.log(3 / 0);
+//.......................................................................
+
+//NaN
+var i = 0 / 0; //no meaningful result
+console.log(i);
+//.......................................................................
+
+//power method
+var j = Math.pow(2, 53);
+console.log(j);
+//.......................................................................
+
+//big number
+var k = Math.pow(2, 2000);
+console.log(k);
+//.......................................................................
+
+//parse a string and return an integer
+var number = "3";
+console.log(parseInt(number));
+console.log(typeof parseInt(number));
+console.log(parseInt("not a number"));
+console.log(typeof parseInt("not a number"));
+//.......................................................................
