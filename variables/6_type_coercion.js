088265f50b8541714550339fa9f32d6f29beb9ca
sudhir kumar <65527808+titanSlayer12@users.noreply.github.com>
2021-07-25 23:00:18 +0530

    Update 6_type_coercion.js

--- variables/6_type_coercion.js
+++ variables/6_type_coercion.js
@@ -3,30 +3,30 @@
 
 
 var z = "1" + 5;
-console.log(z);
-console.log(typeof z);
+console.log(z); // 15
+console.log(typeof z); // string
 
 z = "1" - 5;
-console.log(z);
-console.log(typeof z);
+console.log(z);  // -4
+console.log(typeof z);  // number
 
-z = "one" * 2;
-console.log(z);
-console.log(typeof z);
+z = "one" * 2; 
+console.log(z);  // NaN
+console.log(typeof z); // number
 
 z = 2 * null;
-console.log(z);
-console.log(typeof z);
+console.log(z); // 0
+console.log(typeof z); // number
 
-console.log(0 == false);
-console.log("" == false);
-console.log(null == false);
-console.log(undefined == false);
+console.log(0 == false);  // true
+console.log("" == false); // true
+console.log(null == false); // false
+console.log(undefined == false);  // false
 
 //undefined == null
-console.log(undefined == null);
-console.log(undefined == undefined);
-console.log(null == null);
+console.log(undefined == null);  // true
+console.log(undefined == undefined);  // true
+console.log(null == null); // true
 
 //NaN == NaN
-console.log(NaN == NaN);
+console.log(NaN == NaN);  // false
