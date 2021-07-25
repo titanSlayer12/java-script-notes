a2011781a8fca97e5ef678a67ab6c15e60429be5
sudhir kumar <65527808+titanSlayer12@users.noreply.github.com>
2021-07-25 22:57:10 +0530

    Update 5_null_and_undefined.js

--- variables/5_null_and_undefined.js
+++ variables/5_null_and_undefined.js
@@ -2,9 +2,9 @@
 
 
 var x;
-console.log(x);
-console.log(typeof x);
+console.log(x); // undefined
+console.log(typeof x); // undefined
 
 var y = null;//null can be assigned as a value as a representation of no value
-console.log(y);
-console.log(typeof y);
+console.log(y); // null
+console.log(typeof y); // object
