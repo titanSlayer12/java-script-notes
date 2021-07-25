12d48633b7ef68d74dc4378da7f40694883bc23a
sudhir kumar <65527808+titanSlayer12@users.noreply.github.com>
2021-07-25 22:55:37 +0530

    Update 4_Booleans.js

--- variables/4_Booleans.js
+++ variables/4_Booleans.js
@@ -7,49 +7,49 @@
 var x = 3;
 var y = 5;
 var z = x < y;
-console.log(z);
-console.log(typeof z);
+console.log(z);  // true
+console.log(typeof z);  // boolean
 //...........................................................
 //Logical Operators
 z = (x < y) && (x > 0);
-console.log(z);
+console.log(z);  // true
 z = false || true;
-console.log(z);
+console.log(z);  // true
 z = !(x < 10);
-console.log(z);
+console.log(z);  // false
 //...........................................................
 //Operator Precedence
 z = false && true || true;//&& stronger
-console.log(z);
+console.log(z); // true
 //...........................................................
 //Ternary Operator
 z = x > 2 ? 'Pass' : 'Fail';
-console.log(z);
+console.log(z); // pass
 //...........................................................
 //Logical operators with non booleans
-console.log('banana' == false);
+console.log('banana' == false); // false
 
 z = 'banana' && 'apple';
-console.log(z);
+console.log(z); // apple
 
 
 z = 'banana' || 'apple';
-console.log(z);
+console.log(z);  // banana
 
 var fruit;
-console.log(fruit);
-console.log(fruit == false);
+console.log(fruit); // undefines
+console.log(fruit == false);  // false
 z = fruit || 'apple';
-console.log(z);
+console.log(z); // apple
 
-console.log('' == false);
+console.log('' == false); // true
 z = '' || 'apple';
-console.log(z);
+console.log(z); // false
 
 z = '' && false;
-console.log(z);
+console.log(z); // <empty string>
 
 z = 0 && false;
-console.log(z);
+console.log(z); // 0
 
 //...........................................................
